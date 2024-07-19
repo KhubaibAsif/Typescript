88c1ef1b8f51b88f31d065714b3b4f6a96c68f98
Khubaiblakhpat <153702821+KhubaibAsif@users.noreply.github.com>
2024-07-20 01:13:20 +0500

    learning typescript

---
+++ index.js
@@ -0,0 +1,19 @@
+var userName = "Khubaib Asif";
+var userAge = 28;
+var userFavDishes = ["Biryani", "Pizza"];
+var userIsMarried = false;
+console.log("userName=>", userName);
+console.log("userAge=>", userAge);
+userIsMarried = false;
+var user = {
+    firstName: "Khubaib",
+    lastName: "Asif",
+    favNumbers: [10, 12],
+    favSubjects: ["English", "Urdu"],
+    age: 20,
+};
+function greet(name) {
+    return "Hello ".concat(name);
+}
+var msg = greet("Khubaib Asif");
+console.log(msg);
